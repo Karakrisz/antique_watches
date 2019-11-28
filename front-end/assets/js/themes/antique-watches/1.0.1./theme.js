7b81566db19a665dc6bf4a74bb08f67597621b14
Krisztian <webevolution13@gmail.com>
2019-11-28 17:36:54 +0100

    apply css instead of javascript
    status active append

--- front-end/assets/js/themes/antique-watches/1.0.1./theme.js
+++ front-end/assets/js/themes/antique-watches/1.0.1./theme.js
@@ -1,12 +1,9 @@
 /* WHEN DOCUMENT READY */
 $(document).ready(function () {
-    Add_Active()
+
 });
 
 /* FUNCTIONS */
-function Add_Active() {
-    $('.header__container__navbar__nav__elem__link').click(function () {
-        $('.header__container__navbar__nav__elem__link').removeClass("status-active");
-        $(this).addClass("status-active");
-    });
-};
+/* function () {
+
+};*/
