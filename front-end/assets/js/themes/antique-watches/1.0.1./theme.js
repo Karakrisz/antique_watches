5740296b78b5b18a1a80eaadd254fa38ff6b5b49
Krisztian <webevolution13@gmail.com>
2020-01-04 19:00:07 +0100

    References elements => footer

--- front-end/assets/js/themes/antique-watches/1.0.1./theme.js
+++ front-end/assets/js/themes/antique-watches/1.0.1./theme.js
@@ -1,9 +1,15 @@
 /* WHEN DOCUMENT READY */
 $(document).ready(function () {
-
+    Mymenu()
 });
 
 /* FUNCTIONS */
-/* function () {
-
-};*/
+function Mymenu() {
+    $(document).on("scroll", function () {
+        if ($(document).scrollTop() > 50) {
+            $(".header__container__box").removeClass("header__container__box__bottom-shadow-off").addClass("header__container__box__bottom-shadow-on");
+        } else {
+            $(".header__container__box").removeClass("header__container__box__bottom-shadow-on").addClass("header__container__box__bottom-shadow-off");
+        }
+    });
+};
