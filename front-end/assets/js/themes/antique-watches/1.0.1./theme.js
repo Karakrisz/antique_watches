9bf01584f8eb366f83a2117d184200af80351b67
Krisztian <webevolution13@gmail.com>
2020-02-04 20:22:43 +0100

    static version
    without pictures

--- front-end/assets/js/themes/antique-watches/1.0.1./theme.js
+++ front-end/assets/js/themes/antique-watches/1.0.1./theme.js
@@ -1,6 +1,7 @@
 /* WHEN DOCUMENT READY */
 $(document).ready(function () {
     Mymenu()
+    MyScroll()
 });
 
 /* FUNCTIONS */
@@ -11,5 +12,32 @@ function Mymenu() {
         } else {
             $(".header__container__box").removeClass("header__container__box__bottom-shadow-on").addClass("header__container__box__bottom-shadow-off");
         }
+    });
+};
+
+function MyScroll() {
+    /*------------------------------------
+        		1. Scroll to .div about us
+        ----------------------------------- */
+    $('a.header__container__navbar__nav__elem__link--about-us-scroll').click(function () {
+        $('html, body').animate({
+            scrollTop: $("div.references__container__box").offset().top - 130
+        }, 1000)
+    });
+    /*------------------------------------
+        		2. Scroll to .div photo gallery
+        ----------------------------------- */
+    $('a.header__container__navbar__nav__elem__link--gallery-scroll').click(function () {
+        $('html, body').animate({
+            scrollTop: $("div.photo-gallery__container").offset().top - 130
+        }, 1000)
+    });
+    /*------------------------------------
+    		3. Scroll to .div relationship
+    ----------------------------------- */
+    $('a.header__container__navbar__nav__elem__link--relationship-scroll').click(function () {
+        $('html, body').animate({
+            scrollTop: $("div.footer__container__content").offset().top - 130
+        }, 1000)
     });
 };
